bc46968a65285f555e0086066db812b782d6c38e
Oleg Zarevych <Oleg.Zarevych@gmail.com>
2018-11-13 20:33:40 +0200

    fix firefox browser

--- SeleniumTestFramework/BrowserFactory/FirefoxBrowser.py
+++ SeleniumTestFramework/BrowserFactory/FirefoxBrowser.py
@@ -4,12 +4,12 @@ from BrowserFactory.AbstractFactory import AbstractFactory
 class FirefoxBrowser(AbstractFactory):
     
     def __init__(self):
-        self.driver = webdriver.Firefox()
+        self.driver = webdriver.Firefox(executable_path="/usr/bin/geckodriver")
         self.driver.maximize_window()
     
     def open(self, url):
         self.driver.get(url)
         
     def close(self):
-        self.driver.close();
+        self.driver.quit();
         
