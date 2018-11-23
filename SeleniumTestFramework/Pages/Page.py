f0b7aec32fb98ac09c00655b05405eca8c7bebbe
Oleg Zarevych <Oleg.Zarevych@gmail.com>
2018-11-23 23:55:00 +0200

    add implicitly wait for 10 seconds

--- SeleniumTestFramework/Pages/Page.py
+++ SeleniumTestFramework/Pages/Page.py
@@ -5,6 +5,7 @@ class Page(object):
     
     def __init__(self, driver):
         self.driver = driver
+        self.driver.implicitly_wait(10)
                 
     def find_element(self, locator):
         element = self.driver.find_element_by_xpath(locator)
