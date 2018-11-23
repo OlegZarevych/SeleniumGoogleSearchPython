0dd44e4a7fae4a4f0434c95c363d3f5a82428561
Oleg Zarevych <Oleg.Zarevych@gmail.com>
2018-11-24 00:48:43 +0200

    update test_google_page test

--- SeleniumTestFramework/Tests/TestGoogleMain.py
+++ SeleniumTestFramework/Tests/TestGoogleMain.py
@@ -6,5 +6,8 @@ class TestGoogleMain(BaseTest):
     
     def test_google_page(self):
         logging.info("Test is started:", self._testMethodName)
+        text = "Python"
         main_page = GoogleMainPage(self.driver)
-        main_page.search_text("qa")
+        result_page = main_page.search_text(text)
+        first_link = result_page.get_first_link()
+        self.assertTrue(text in first_link.text, "No searched text in first link")
